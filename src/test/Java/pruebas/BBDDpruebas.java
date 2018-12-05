c963dbc3d0e4a4e6ac57b2c6dd889794dbdab86c
celeste <celeste@172.19.179.212>
2018-12-05 14:02:04 +0100

    insertar

--- src/test/Java/pruebas/BBDDpruebas.java
+++ src/test/Java/pruebas/BBDDpruebas.java
@@ -1,4 +1,5 @@
 package pruebas;
+
 import static org.junit.Assert.*;
 
 import org.bson.Document;
@@ -13,64 +14,64 @@ import com.mongodb.client.MongoDatabase;
 
 import modelo.mongodb.DBBroker;
 
-
 public class BBDDpruebas {
 
 	private static MongoClient cliente = null;
 	private static MongoDatabase bd = null;
 	private MongoCursor<Document> elementos;
 	private static MongoCollection<Document> dbEmpleadoTest;
-	
+
 	// Collecciones
 	private String TestUsuarios = null;
 
 	public BBDDpruebas() {
 
-		
 	}
-	public static MongoCollection<Document> getCollection(String nombreColeccion){
+
+	public static MongoCollection<Document> getCollection(String nombreColeccion) {
 		String acceso = "mongodb://usuarioGeneral:usuarioGeneral2018@ds233763.mlab.com:33763/avengerslive";
 		MongoClientURI uri = new MongoClientURI(acceso);
 		cliente = new MongoClient(uri);
 		bd = cliente.getDatabase("avengerslive");
 		return bd.getCollection(nombreColeccion);
-		
-		
+
 	}
-	
+
 	@Test
-	
+
 	public void testInsertarEmpleado() {
-			
-		MongoCollection<Document> testEmpleado=getCollection("Empleados");
-
-		
+		MongoCollection<Document> testEmpleado = getCollection("Empleado");
+		Document doc = new Document();
+		doc.append("_id", "01");
+		doc.append("email", "c");
+		doc.append("contrasena", "c");
+		doc.append("nombre", "c c c");
+		doc.append("rol", "c");
+		DBBroker db = new DBBroker();
+		db.insertarDocumento(testEmpleado, doc);
+		FindIterable<Document> ite = testEmpleado.find(doc);
+		assertTrue(ite.iterator().hasNext());
 	}
-	
-
 
 	public void testEliminarEmpleado() {
-		
+
 		MongoCollection<Document> testEmpleado = getCollection("Empleados");
-		
+
 		Document doc = new Document();
-		
+
 		testEmpleado.insertOne(doc);
 		doc.append("email", "pruebaenrique@hotmail.com");
 		doc.append("contrasena", "Enrique1234");
 		doc.append("nombre", "Enrique");
 		doc.append("rol", "administrador");
-		
-		DBBroker db= new DBBroker();
-		
-		
-	}
-	
-	public void testAbrirFichaje() {
-		
-		MongoCollection<Document> testEmpleado=getCollection("Fichajes");
 
-		
+		DBBroker db = new DBBroker();
+
+	}
+
+	public void testAbrirFichaje() {
+
+		MongoCollection<Document> testEmpleado = getCollection("Fichajes");
+
 	}
 }
-
